47ffc03fb2cd5d266bb0ff5d9b3a5a25cbe37d7d
Vladimir <veb455@mail.ru>
2024-12-27 10:23:35 +0300

    converter git commit -m converter git add . git commit -m converter

---
+++ src/utils/converter.ts
@@ -0,0 +1,23 @@
+const converter = (newCurrency, prevCurrency, prevValue) => {
+    const rates = {
+        USD: {
+            RUB: 75,
+            EUR: 0.85
+        },
+        RUB: {
+            USD: 0.0133,
+            EUR: 0.0113
+        },
+        EUR: {
+            USD: 1.18,
+            RUB: 88.30
+        }
+    };
+
+    const conversionRate = rates[prevCurrency][newCurrency];
+    const newValue = prevValue * conversionRate;
+
+    return Math.round(newValue);
+};
+
+export default converter
